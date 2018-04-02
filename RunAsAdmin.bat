072c020b237db5a716ecf4f46b9622840cbacb18
Caleb Bennett <calebinfo270@gmail.com>
2018-04-02 10:14:39 -0500

    Added RunAsAdmin.bat

---
+++ RunAsAdmin.bat
@@ -0,0 +1,3 @@
+rem Runs the script as Admin to enable privileges to kill sessions.
+rem Change python location and script location to fit your environment.
+runas /user:capc\Administrator "<Path to Python> <Path to __main__.py>"
